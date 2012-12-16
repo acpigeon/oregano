e54d3c80e33d96234258457c3bb74a1f5e062080
Aaron Pigeon <acpigeon@gmail.com>
2012-12-16 17:29:08 -0600

    test authorization

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,5 @@
-print("Hello World!"
+from libgreader import GoogleReader, ClientAuthMethod, Feed
+auth = ClientAuthMethod('acpigeon', 'Katana666')
+reader = GoogleReader(auth)
+print reader.getUserInfo()
 
-print("mmm oregano")
